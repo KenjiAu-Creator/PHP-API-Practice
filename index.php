6b3d98123f0d1a581462e3f9f9df7c8f45c6711d
Kenji <kenji.au9@gmail.com>
2020-10-05 16:59:12 -0600

    Added in initial API call + form;

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <?php
 $GLOBALS['pageTitle'] = "Home";
 
+// If the user has submitted a artist query
+if (isset($_POST['artist'] ) )
+{ // Request the artist data from the API.
+  $artistDataString = file_get_contents(
+  "https://rest.bandsintown.com/artists/{$_POST['artist']}?app_id=1"
+  );
+}
+
 // Show header
 include './templates/header.php';
 ?>
@@ -13,4 +21,8 @@ include './templates/header.php';
   <input type="submit" value="Search">
 </form>
 
+<h2>Debugging</h2>
+<?php if ($artistDataString) : ?>
+  <?php echo $artistDataString ?>
+<?php endif ?>
 <?php include './templates/footer.php';
